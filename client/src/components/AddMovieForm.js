bea86d6bb652ace7b9bd5360f8baf894b33e5c02
Bradly <bradlylewisdev@gmail.com>
2021-08-19 00:20:52 -0400

    update mvp

--- client/src/components/AddMovieForm.js
+++ client/src/components/AddMovieForm.js
@@ -1,12 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { useParams, useHistory } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 import axios from 'axios';
 
-const EditMovieForm = (props) => {
+const AddMovieForm = (props) => {
 	const { push } = useHistory();
-	const { id } = useParams();
 
 	const [movie, setMovie] = useState({
 		title:"",
@@ -15,17 +14,9 @@ const EditMovieForm = (props) => {
 		metascore: 0,
 		description: ""
 	});
-
-	useEffect(() => {
-		axios.get(`http://localhost:5000/api/movies/${id}`)
-			.then(res => {
-				setMovie(res.data);
-			})
-			.catch(err => {
-				console.log(err)
-			})
-	}, []);
 	
+
+
 	const handleChange = (e) => {
         setMovie({
             ...movie,
@@ -35,14 +26,12 @@ const EditMovieForm = (props) => {
 
     const handleSubmit = (e) => {
 		e.preventDefault();
-		axios.put(`http://localhost:5000/api/movies/${id}`, movie)
-			.then(res => {
-				props.setMovies(res.data);
-				push(`/movies/${id}`)
-			})
-			.catch(err => {
-				console.log(err)
-			})
+		axios.post(`http://localhost:5000/api/movies/`, movie)
+		.then(res=> {
+			props.setMovies(res.data)
+			push(`/movies`)
+            console.log("ADD NEW MOVIE", res.data)
+		})
 	}
 	
 	const { title, director, genre, metascore, description } = movie;
@@ -86,4 +75,4 @@ const EditMovieForm = (props) => {
 	</div>);
 }
 
-export default EditMovieForm;
+export default AddMovieForm;
